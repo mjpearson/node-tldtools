a3b2ff3ea10400d6223f114f547aa710ba29c1fd
michael pearson <gravatar@x0rz.com>
2012-11-10 11:57:53 -0500

    getDomain helper fix, was breaking for mailto://

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
 Thanks for using tldtools, it's distributed under the MIT License (MIT)
 
-Copyright (c) 2011 Michael Pearson <ossdev@cloudspark.freshbutter.me>
+Copyright (c) 2011 Michael Pearson <npm@m.bip.io>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in the
@@ -308,7 +308,7 @@ TLD_TOOLS = {
                    return (this.domain != '' && this.tld != '');
                },
                'getDomain' : function() {
-                   return this.domain + '.' + this.tld;
+                   return urlTokens.hostname;
                }
            }
        };
